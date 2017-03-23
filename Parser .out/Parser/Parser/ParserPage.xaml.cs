b833ea73a50d4df7617e134a439f759efe380f2e
veNqus <Azragot@gmail.com>
2017-03-23 12:20:18 +0100

    Podstawowy parser out

---
+++ Parser .out/Parser/Parser/ParserPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Parser
+{
+    /// <summary>
+    /// Interaction logic for ParserPage.xaml
+    /// </summary>
+    public partial class ParserPage : Page
+    {
+        int value1 = 1;
+        int value2 = 2;
+            Regex parts = new Regex(@"( *\( +\d+\, +\d+\) +-*\d.\d+\/)+"); 
+      //  Regex parts = new Regex(@"( *\( +\b(value1)\, +\d+\) +-*\d.\d+\/)+");
+        StreamReader reader = new StreamReader("PlikTestowy.txt.txt");
+        public ParserPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Match match = parts.Match(line);
+                if (match.Success)
+                {
+                    listBox.Items.Add(match);
+                }
+            }
+        }
+    }
+}
